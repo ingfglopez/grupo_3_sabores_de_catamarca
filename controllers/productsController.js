2d61ddf37a27eeaa1be3ac62871a7facce04d2ab
diego50to <diegofernandosoto@gmail.com>
2023-12-16 14:30:56 -0300

    Mostrar todos los Productos - Detalle y Create

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,28 +1,48 @@
-const categories = require('../data/categories.json')
+const categories = require("../data/categories.json");
+const path = require("node:path");
+const fs = require("node:fs");
+
+const productsFilePath = path.join(__dirname, "../data/products.json");
+const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const productsController = {
-
   products: (req, res) => {
-    res.render('products/products')
+    res.render("products/products", { data: products });
   },
+
   detail: (req, res) => {
-    // buscar product
-    res.render('products/productDetail')
+    const { id } = req.params;
+    const productDetail = products.find((p) => p.id === Number(id));
+    res.render("products/productDetail", { data: productDetail });
   },
+
   create: (req, res) => {
-    if (req.method == 'GET') {
-      res.render('/products/productForm', {
-        categories
-      })
-    }    
+    res.render("products/productForm");
   },
+
+  store: (req, res) => {
+    const nameImage = req.file.filename;
+    const { name, description, category, weight, stock, price } = req.body;
+    const newProduct = {
+      id: products.length + 1,
+      name,
+      description,
+      image: nameImage,
+      category,
+      weight,
+      stock,
+      price,
+    };
+    products.push(newProduct);
+    res.redirect("products");
+  },
+
   update: (req, res) => {
     //
   },
   delete: (req, res) => {
     //
   },
-}
+};
 
 module.exports = productsController;
-
--- routes/products.js
+++ routes/products.js
@@ -1,21 +1,38 @@
-const express = require('express')
+const express = require("express");
 const router = express.Router();
-const productController = require('../controllers/productsController');
+const productController = require("../controllers/productsController");
+
+const path = require("path");
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    const folder = "public/images";
+    cb(null, folder);
+  },
+  filename: (req, file, cb) => {
+    const imageName =
+      file.fieldname + "-" + Date.now() + path.extname(file.originalname);
+    cb(null, imageName);
+  },
+});
+
+const upload = multer({ storage: storage });
 
 // Create
-router.get('/create', productController.create);
-router.post('/', productController.create);
+router.get("/create", productController.create);
+router.post("/", upload.single("image"), productController.store);
 
 // Read
-router.get('/', productController.products);
-router.get('/:id', productController.detail);
+router.get("/", productController.products);
+router.get("/:id", productController.detail);
 
 // Update
-router.get('/:id/edit', productController.update);
-router.put('/:id', productController.update);
+router.get("/:id/edit", productController.update);
+router.put("/:id", productController.update);
 
 // Delete
-router.get(':id/delete', productController.delete)
-router.delete(':id', productController.delete)
+router.get(":id/delete", productController.delete);
+router.delete(":id", productController.delete);
 
 module.exports = router;
