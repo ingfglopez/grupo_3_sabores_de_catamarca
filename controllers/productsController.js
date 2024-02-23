73e8254c4b75096e602286ac166adbbcba570d56
diego50to <diegofernandosoto@gmail.com>
2024-02-23 15:54:37 -0300

    CRUD User Edit Terminado

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,43 +1,48 @@
 const path = require("node:path");
 const fs = require("node:fs");
-const db = require('../database/models/index');
+const db = require("../database/models/index");
 
 //const productsFilePath = path.join(__dirname, "../data/products.json");
 //const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 //const categories = require("../data/categories.json");
 
 const productsController = {
-
   products: (req, res) => {
-    db.Product.findAll().then(products => {
-      res.render("products/products", { products })
-    }).catch(error => {
-      res.send(error);
-    })
+    db.Product.findAll()
+      .then((products) => {
+        res.render("products/products", { products });
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   },
 
   detail: (req, res) => {
     const { id } = req.params;
     //const productDetail = products.find((p) => p.id === Number(id));
-    db.Product.findByPk(req.params.id).then(product => {
-      res.render("products/productDetail", { product });
-    }).catch(error => {
-      res.send(error);
-    })
+    db.Product.findByPk(req.params.id)
+      .then((product) => {
+        res.render("products/productDetail", { product });
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   },
 
   create: (req, res) => {
-    db.Category.findAll().then(categories => {
-      const product = {};
-      res.render("products/productForm", {
-        product,
-        categories,
-        action: "/products",
-        title: "Nuevo producto",
+    db.Category.findAll()
+      .then((categories) => {
+        const product = {};
+        res.render("products/productForm", {
+          product,
+          categories,
+          action: "/products",
+          title: "Nuevo producto",
+        });
+      })
+      .catch((error) => {
+        res.send(error);
       });
-    }).catch(error => {
-      res.send(error);
-    })
   },
 
   store: (req, res) => {
@@ -53,41 +58,47 @@ const productsController = {
       price,
     };
 
-    db.Product.create(newProduct).then(product => {
-      console.log('Producto creado', product);
-      res.redirect("products");
-    }).catch(error => {
-      res.send(error);
-    })
-
+    db.Product.create(newProduct)
+      .then((product) => {
+        console.log("Producto creado", product);
+        res.redirect("products");
+      })
+      .catch((error) => {
+        res.send(error);
+      });
   },
 
   update: (req, res) => {
     //const product = products.find((producto) => producto.id == req.params.id);
-    
+
     if (req.method == "GET") {
-      console.log('Muestra form edit...');
-      const product = db.Product.findByPk(req.params.id).then(product => {
-        db.Category.findAll().then(categories => {
-          res.render("products/productForm", {
-            product,
-            categories,
-            action: `/products/${product.id}?_method=PUT`,
-            title: "Editar producto",
-          });          
-        }).catch(error => {
-          res.send(error);
+      console.log("Muestra form edit...");
+      const product = db.Product.findByPk(req.params.id)
+        .then((product) => {
+          db.Category.findAll()
+            .then((categories) => {
+              res.render("products/productForm", {
+                product,
+                categories,
+                action: `/products/${product.id}?_method=PUT`,
+                title: "Editar producto",
+              });
+            })
+            .catch((error) => {
+              res.send(error);
+            });
         })
-      }).catch(error => {
-        res.send(error);
-      })
+        .catch((error) => {
+          res.send(error);
+        });
     } else {
       //const indiceProducto = products.findIndex(
       //  (producto) => producto.id == req.params.id
       //);
-      db.Product.findByPk(req.params.id).then(product => {
+      db.Product.findByPk(req.params.id).then((product) => {
         const image = req.file ? req.file.filename : product.image;
-        const { name, description, category_id, weight, stock, price } = req.body;
+        const { name, description, category_id, weight, stock, price } =
+          req.body;
         const id = Number(req.params.id);
         const productEditado = {
           id,
@@ -102,24 +113,28 @@ const productsController = {
 
         db.Product.update(productEditado, {
           where: {
-            id: req.params.id
-          }
-        }).then(product => {
-          console.log('Producto modificado', product);
-          res.redirect("/products");
-        }).catch(error => {
-          res.send(error);
+            id: req.params.id,
+          },
         })
-      })
+          .then((product) => {
+            console.log("Producto modificado", product);
+            res.redirect("/products");
+          })
+          .catch((error) => {
+            res.send(error);
+          });
+      });
     }
   },
   delete: (req, res) => {
     if (req.method == "GET") {
-      db.Product.findByPk(req.params.id).then(product => {
-        res.render("products/product-delete", { product });
-      }).catch(error => {
-        res.send(error);
-      })
+      db.Product.findByPk(req.params.id)
+        .then((product) => {
+          res.render("products/product-delete", { product });
+        })
+        .catch((error) => {
+          res.send(error);
+        });
     } else {
       //const indiceProducto = products.findIndex(
       //  (producto) => producto.id == req.params.id
@@ -128,13 +143,15 @@ const productsController = {
       //fs.writeFileSync("data/products.json", JSON.stringify(products), "utf-8");
       db.Product.destroy({
         where: {
-          id: req.params.id
-        }
-      }).then(result => {
-        res.redirect("/products");
-      }).catch(error => {
-        res.send(error);
+          id: req.params.id,
+        },
       })
+        .then((result) => {
+          res.redirect("/products");
+        })
+        .catch((error) => {
+          res.send(error);
+        });
     }
   },
 };
