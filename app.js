fb1f36842c262281d81468fe7fc0f0cb889adaa6
diego50to <diegofernandosoto@gmail.com>
2024-03-31 01:35:42 -0300

    Configuracion CORS

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const path = require("path");
 const methodOverride = require("method-override");
-const session = require('express-session');
-const cookieParser = require('cookie-parser');
-const flash = require('connect-flash');
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const flash = require("connect-flash");
+const cors = require("cors");
 
 const mainRouter = require("./routes/main");
 const productsRouter = require("./routes/productsRouter");
 const usersRouter = require("./routes/usersRouter");
-const adminRouter = require('./routes/adminRouter');
+const adminRouter = require("./routes/adminRouter");
 
 const apiUsersRouter = require("./routes/api/users");
 const apiProductsRouter = require("./routes/api/products");
@@ -24,21 +25,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(methodOverride("_method"));
+app.use(cors({ origin: "http://localhost:5173" }));
 
 // Middleware de session
-app.use(session({
-  secret: '123456',
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: "123456",
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 // Añadir la funcionalidad de mensajes flash
 app.use(flash());
 
 app.use((req, res, next) => {
   res.locals.mensajes = req.flash(); // Los mensajes flash pasan a estar disponibles para toda la aplicacion
-  next()
-})
+  next();
+});
 
 // Determinar si se recordo al usuario al momento de loguear
 app.use(usernameInCookie);
@@ -51,7 +55,7 @@ app.set("view engine", "ejs");
 
 // Rutas
 app.use("/", mainRouter);
-app.use('/admin/', adminRouter);
+app.use("/admin/", adminRouter);
 app.use("/products", productsRouter);
 app.use("/users", usersRouter);
 
